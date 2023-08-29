ea8961aa423644d40571ef401f601aff32104cbb
pedroc07 <phvcruz@ecomp.uefs>
2023-08-29 12:51:07 -0300

    leitura de tags

--- read.py
+++ read.py
@@ -1,2 +1,29 @@
+import mercury
+import sys
+from datetime import datetime
+
+
+param = 2300
+
+if len(sys.argv) > 1:
+        param = int(sys.argv[1])
+
+# configura a leitura na porta serial onde esta o sensor
+reader = mercury.Reader("tmr:///dev/ttyUSB0")
+
+# para funcionar use sempre a regiao "NA2" (Americas)
+reader.set_region("NA2")
+
+# nao altere a potencia do sinal para nao prejudicar a placa
+reader.set_read_plan([1], "GEN2", read_power=param)
+
+# realiza a leitura das TAGs proximas e imprime na tela
+# print(reader.read())
 def retorna_tags():
-    return ["E20000172211011118905471"]
+    epcs = map(lambda tag: tag, reader.read())
+    tags = []
+    for tag in epcs:
+        print(tag.epc, tag.read_count, tag.rssi, datetime.fromtimestamp(tag.timestamp))
+        tags.append(tag.epc)
+
+    return tags
