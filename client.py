ea6a8541a115e62d94161298a59aa72bb27eda18
pedroc07 <phvcruz@ecomp.uefs>
2023-08-29 13:24:31 -0300

    broken pipe fix

--- client.py
+++ client.py
@@ -1,5 +1,5 @@
 import socket
-from leitor import retorna_tags
+#from leitor import retorna_tags
 # ip público caso não seja a mesma máquina
 def client(host = 'localhost', port=8099):
     # protocolo tcp
@@ -7,8 +7,12 @@ def client(host = 'localhost', port=8099):
     print (f"Conectando ao {host} port {port}") 
     sock.connect((host, port))
     try: 
-        tags = retorna_tags()
+        tags = ["E20000172211011718905474", "E20000172211010218905459", "E2000017221100961890544A"]
         for mensagem in tags:
+            # protocolo tcp
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+            print (f"Conectando ao {host} port {port}") 
+            sock.connect((host, port))
             print (f"Enviando {mensagem}") 
             sock.send(mensagem.encode('utf-8')) 
             data = sock.recv(2048) 
@@ -22,3 +26,4 @@ def client(host = 'localhost', port=8099):
         sock.close() 
 
 client()
+ 
