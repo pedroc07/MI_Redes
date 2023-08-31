be6ef727a278d9fd33e728e21d802c6c96c2bce4
pedroc07 <phvcruz@ecomp.uefs>
2023-08-31 13:54:48 -0300

    lista de compras

--- client.py
+++ client.py
@@ -2,25 +2,30 @@ import socket
 #from leitor import retorna_tags
 # ip público caso não seja a mesma máquina
 def client(host = 'localhost', port=8099):
-    # protocolo tcp
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-    print (f"Conectando ao {host} port {port}") 
-    sock.connect((host, port))
+    compras = ""
     try: 
         tags = ["E20000172211011718905474", "E20000172211010218905459", "E2000017221100961890544A"]
         for mensagem in tags:
-            print (f"Enviando {mensagem}") 
+            # protocolo tcp
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+            print (f"Conectando ao {host} port {port}\n ") 
+            sock.connect((host, port))
+            print (f"Enviando {mensagem}\n") 
             sock.send(mensagem.encode('utf-8')) 
-            data = sock.recv(2048) 
-            print (f"Lista de compras:\n{data.decode('utf-8')}")
-            confirm = input("Deseja confirmar  compra? ")
+            data = sock.recv(2048)
+            compras += data.decode('utf-8') + "\n"
     except socket.error as e: 
         print (f"Socket error: {e}") 
     except Exception as e: 
         print (f"Other exception: {e}") 
-    finally: 
-        print ("Fechando  conexão com o servidor...") 
-        sock.close() 
+    finally:
+        print ("Fechando  conexão com o servidor...\n") 
+        sock.close()
+        print (f"Lista de compras:\n{compras}")
+        confirm = input("Deseja confirmar  compra? S/N ")
+        if confirm.upper() == "S":
+            # exclui tags
+            pass
 
 sair = False
 while not sair:
